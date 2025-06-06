f6417de83495288e8ea97a1cc5eb70eb3b1361e0
Sanjiv <sanjivsarvaesh30@gmail.com>
2025-06-06 17:15:45 +0100

    intial Commit

---
+++ smoke_test.py
@@ -0,0 +1,16 @@
+from diffusers import StableDiffusionPipeline
+import torch, pathlib
+
+pipe = StableDiffusionPipeline.from_pretrained(
+    "stabilityai/stable-diffusion-2-1-base",
+    torch_dtype=torch.float16
+).to("cuda")
+
+pipe.enable_attention_slicing()
+
+prompt = "a futuristic neon tiger, concept art, 4k"
+image = pipe(prompt, height=512, width=512).images[0]
+
+pathlib.Path("outputs").mkdir(exist_ok=True)
+image.save("outputs/tiger.png")
+
